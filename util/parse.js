c052cac8be59ab8b1565b18ed212ed33c4680761
Will Ginsberg <will.j.ginsberg@gmail.com>
2022-08-22 17:19:30 -0400

    add table html parser utl

--- util/parse.js
+++ util/parse.js
@@ -1,3 +1,26 @@
+import { parse } from "node-html-parser"
+
 export function parseStatusPage(text) {
-    return []
+    const parsed = parse(text)
+    const rows = parsed.querySelector("table>tbody").removeWhitespace().childNodes
+
+    const re = /(.+?)(\d\d\d\d-\d\d-\d\d)?(Arr:)(Dep:)?(\d\d:\d\d)(\d\d:\d\d)(\d\d:\d\d)?(\d\d:\d\d)?/
+    const results = rows
+        .map(row => {
+            const result = re.exec(row.innerText)?.slice(1) || []
+            const [stationName, _date, didArrive, didDepart, ...times] = result
+
+            // Order of values in the table changes for final station
+            const [expectedArrival, actualArrival] = didDepart
+                ? [times[0], times[2]]
+                : [times[0], times[1]]
+
+            return { stationName, expectedArrival, actualArrival }
+        })
+        .filter(({ stationName, expectedArrival, actualArrival }) => {
+            const fields = [stationName, expectedArrival, actualArrival]
+            return fields.every(x => !!x)
+        })
+
+    return results
 }
--- util/parse.test.js
+++ util/parse.test.js
@@ -1,6 +1,29 @@
 import { deepStrictEqual } from 'assert/strict'
-
+import { readFileSync } from 'fs';
+import path from 'path'
+import { fileURLToPath } from 'url'
 import { parseStatusPage } from "./parse.js";
+
+const dirname = path.dirname(fileURLToPath(import.meta.url))
+
+{
+    const html = readFileSync(path.resolve(dirname, './parse.test.html'))
+
+    const actual = parseStatusPage(html)
+    const expected = ([
+        { stationName: 'FALLOWFIELD RAIL STATION', expectedArrival: '12:02', actualArrival: '12:01' },
+        { stationName: 'SMITHS FALLS', expectedArrival: '12:34', actualArrival: '12:33' },
+        { stationName: 'BROCKVILLE', expectedArrival: '13:09', actualArrival: '13:09' },
+        { stationName: 'GANANOQUE', expectedArrival: '13:36', actualArrival: '13:49' },
+        { stationName: 'KINGSTON', expectedArrival: '13:57', actualArrival: '14:11' },
+        { stationName: 'BELLEVILLE', expectedArrival: '14:40', actualArrival: '15:23' },
+        { stationName: 'COBOURG', expectedArrival: '15:20', actualArrival: '16:01' },
+        { stationName: 'OSHAWA', expectedArrival: '15:53', actualArrival: '16:57' },
+        { stationName: 'TORONTO UNION STATION', expectedArrival: '16:33', actualArrival: '17:46' }
+    ])
+
+    deepStrictEqual(actual, expected)
+}
 
 {
     const html = `
@@ -64,7 +87,7 @@ import { parseStatusPage } from "./parse.js";
     `
 
     const actual = parseStatusPage(html)
-    const expected = ([{ station_name: 'TEST STATION NAME', expectedArrival: '18:12', actualArrival: '18:13' }])
+    const expected = ([{ stationName: 'TEST STATION NAME', expectedArrival: '18:12', actualArrival: '18:13' }])
 
     deepStrictEqual(actual, expected)
 }
