0ad923b0156aa378a0f44e157902ce51126df50d
Will Ginsberg <will.j.ginsberg@gmail.com>
2023-07-22 16:49:30 -0400

    react react frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,132 @@
+import { createFilterOptions } from '@mui/material/Autocomplete';
+import React, { useEffect, useState } from 'react';
+import Card from "./Card"
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import Container from '@mui/material/Container';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import Autocomplete from "@mui/material/Autocomplete"
+import Grid from '@mui/material/Grid';
+import TextField from '@mui/material/TextField'
+
+const TORONTO = "TORONTO UNION STATION"
+
+function App() {
+  const darkTheme = createTheme({
+    palette: {
+      mode: 'dark',
+    },
+  });
+
+  const [records, setRecords] = useState([])
+  const [arrivalStationInputValue, setArrivalStationInputValue] = useState("")
+  const [originStationInputValue, setOriginStationInputValue] = useState("")
+  const [trainNumberInputValue, setTrainNumberInputValue] = useState("")
+
+  useEffect(() => {
+    async function fetchData () {
+      const text = await fetch("data.csv")
+        .then(file => file.text())
+      const rows = text.split("\n").map(row => row.split(","))
+
+      setRecords(rows).slice(1, 100)
+    }
+    fetchData()
+  }, [])
+
+  const destinations = records.reduce((acc, next) => {
+    const origin = next[2]
+    const station = next[1]
+    const destinationSet = acc[origin] || new Set()
+    destinationSet.add(station)
+
+    return {
+      ...acc,
+      [origin]: destinationSet
+    }
+  }, {})
+
+  const stationSet = new Set()
+  for (const destinationList of Object.values(destinations)) {
+    for (const station of destinationList) {
+      stationSet.add(station)
+    }
+  }
+  
+  const allStations = [...stationSet].sort((a, b) => {
+    if (a === TORONTO) return -1
+    if (a < b) return -1
+    return 1
+  })
+
+  const trainNumbers = [...records.reduce((acc, next) => {
+    const trainNumber = next[0]
+    acc.add(trainNumber)
+    return acc
+  }, new Set())].sort()
+
+  const results = records
+    .filter(record => {
+      const trainNumber = record[0]
+      const station = record[1]
+      const origin = record[2]
+
+      if (arrivalStationInputValue && station !== arrivalStationInputValue) return false
+      if (originStationInputValue && origin !== originStationInputValue) return false
+      if (trainNumberInputValue && trainNumber !== trainNumberInputValue) return false
+
+      return true
+    })
+    
+
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <Container maxWidth="md">
+          <Grid container component="form" spacing={2} paddingTop={2}>
+            <Grid item xs={12} sm={4}>
+              <Autocomplete
+                options={allStations}
+                inputValue={arrivalStationInputValue}
+                onInputChange={(e, value) => setArrivalStationInputValue(value)}
+                renderInput={(params) => <TextField {...params} label="Arrival Station" />}
+              />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Autocomplete
+                options={Object.keys(destinations)}
+                inputValue={originStationInputValue}
+                onInputChange={(e, value) => setOriginStationInputValue(value)}
+                renderInput={(params) => <TextField {...params} label="Origin Station" />}
+              />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Autocomplete
+                inputValue={trainNumberInputValue}
+                onInputChange={(e, value) => setTrainNumberInputValue(value)}
+                filterOptions={createFilterOptions({ matchFrom: "start" })}
+                options={trainNumbers}
+                renderInput={(params) => <TextField {...params} label="Train Number" />}
+              />
+            </Grid>
+          </Grid>
+          <List>
+            {results.map((row) => (
+              <ListItem key={row}>
+                <Card
+                  trainNumber={row[0]}
+                  station={row[1]}
+                  origin={row[2]}
+                  minutesLate={Number(row[6])}
+                  date={row[3]}
+                />
+              </ListItem>
+            ))}
+          </List>
+      </Container>
+    </ThemeProvider>
+  );
+}
+
+export default App;
---
+++ frontend/src/Card.js
@@ -0,0 +1,35 @@
+import React from "react"
+import { formatDistanceToNow } from 'date-fns';
+import Card from "@mui/material/Card"
+import CardContent from "@mui/material/CardContent"
+import Typography from "@mui/material/Typography"
+
+export default function (props) {
+    const { trainNumber, station, origin, minutesLate, date } = props
+
+    const delayMessage = minutesLate < 0
+        ? `${-minutesLate} minutes early`
+        : minutesLate === 0
+            ? `On time`
+            : `${minutesLate} minutes late`
+
+    const color = minutesLate < 10
+        ? "green"
+        : minutesLate < 25
+            ? "yellow"
+            : "red"
+
+    return (
+        <Card sx={{ borderLeft: `4px solid ${color}`, width: "100%" }}>
+            <CardContent>
+                <Typography component="span" noWrap>{origin}</Typography>
+                <span> - </span>
+                <Typography component="span" noWrap><strong>{station}</strong></Typography>
+                <Typography>{delayMessage}</Typography>
+                <Typography>Train {trainNumber}</Typography>
+                <Typography>{formatDistanceToNow(new Date(date))}</Typography>
+                <Typography>{date}</Typography>
+            </CardContent>
+        </Card>
+    )
+}
