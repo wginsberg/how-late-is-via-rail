d690332512f3d7552f5f46d6e7c305bed2f0fd6d
Will Ginsberg <will.j.ginsberg@gmail.com>
2023-07-27 12:26:17 -0400

    cleanup util

--- frontend/src/util.test.js
+++ frontend/src/util.test.js
@@ -20,7 +20,7 @@ describe("getStats", () => {
         expect(actual.filteredRecords).toEqual(records)
     })
 
-    it("filters records when station supplied", () => {
+    it("filters records when arrival station supplied", () => {
         const records = [
             [44, "OSHAWA", "TORONTO UNION STATION", "2023-07-21", "14:53", "15:00", 7],
             [44, "KINGSTON", "TORONTO UNION STATION", "2023-07-21", "16:34", "16:47", 13],
@@ -44,7 +44,7 @@ describe("getStats", () => {
             [645, "KINGSTON", "OTTAWA", "2022-08-26", "16:34", "16:52", 18]
         ]
 
-        const actual = getStats(records, "", "OTTAWA")
+        const actual = getStats(records, "KINGSTON", "OTTAWA")
 
         const expectedRecords = [
             [645, "KINGSTON", "OTTAWA", "2022-08-26", "16:34", "16:52", 18]
@@ -57,14 +57,14 @@ describe("getStats", () => {
         const records = [
             [44, "OSHAWA", "TORONTO UNION STATION", "2023-07-21", "14:53", "15:00", 7],
             [44, "KINGSTON", "TORONTO UNION STATION", "2023-07-21", "16:34", "16:47", 13],
+            [66, "KINGSTON", "TORONTO UNION STATION", "2022-08-26", "17:37", "17:51", 14],
             [645, "KINGSTON", "OTTAWA", "2022-08-26", "16:34", "16:52", 18]
         ]
 
-        const actual = getStats(records, "", "", 44)
+        const actual = getStats(records, "KINGSTON", "TORONTO UNION STATION", 44)
 
         const expectedRecords = [
-            [44, "OSHAWA", "TORONTO UNION STATION", "2023-07-21", "14:53", "15:00", 7],
-            [44, "KINGSTON", "TORONTO UNION STATION", "2023-07-21", "16:34", "16:47", 13]
+            [44, "KINGSTON", "TORONTO UNION STATION", "2023-07-21", "16:34", "16:47", 13],
         ]
 
         expect(actual.filteredRecords).toEqual(expectedRecords)
@@ -90,7 +90,7 @@ describe("getStats", () => {
         expect(actual.availableTrainNumbers).toEqual(expectedTrainNumbers)
     })
 
-    it("filters other options when station supplied", () => {
+    it("filters other origins when station supplied", () => {
         const records = [
             [44, "OSHAWA", "TORONTO UNION STATION", "2023-07-21", "14:53", "15:00", 7],
             [44, "KINGSTON", "TORONTO UNION STATION", "2023-07-21", "16:34", "16:47", 13],
@@ -103,44 +103,6 @@ describe("getStats", () => {
         const expectedStations = ["FALLOWFIELD", "KINGSTON", "OSHAWA"]
         const expectedOrigins = ["TORONTO UNION STATION"]
         const expectedTrainNumbers = [44, 52]
-
-        expect(actual.availableStations).toEqual(expectedStations)
-        expect(actual.availableOrigins).toEqual(expectedOrigins)
-        expect(actual.availableTrainNumbers).toEqual(expectedTrainNumbers)
-    })
-
-    it("filters other options when origin supplied", () => {
-        const records = [
-            [44, "OSHAWA", "TORONTO UNION STATION", "2023-07-21", "14:53", "15:00", 7],
-            [44, "KINGSTON", "TORONTO UNION STATION", "2023-07-21", "16:34", "16:47", 13],
-            [645, "FALLOWFIELD", "OTTAWA", "2023-06-24", "14:54", "15:01", 7],
-            [52, "KINGSTON", "TORONTO UNION STATION", "2023-06-23", "10:57", "11:10", 13]
-        ]
-
-        const actual = getStats(records, "", "OTTAWA")
-
-        const expectedStations = ["FALLOWFIELD"]
-        const expectedOrigins = ["OTTAWA", "TORONTO UNION STATION"]
-        const expectedTrainNumbers = [645]
-
-        expect(actual.availableStations).toEqual(expectedStations)
-        expect(actual.availableOrigins).toEqual(expectedOrigins)
-        expect(actual.availableTrainNumbers).toEqual(expectedTrainNumbers)
-    })
-
-    it("filters other options when train number supplied", () => {
-        const records = [
-            [44, "OSHAWA", "TORONTO UNION STATION", "2023-07-21", "14:53", "15:00", 7],
-            [44, "KINGSTON", "TORONTO UNION STATION", "2023-07-21", "16:34", "16:47", 13],
-            [645, "FALLOWFIELD", "OTTAWA", "2023-06-24", "14:54", "15:01", 7],
-            [52, "KINGSTON", "TORONTO UNION STATION", "2023-06-23", "10:57", "11:10", 13]
-        ]
-
-        const actual = getStats(records, "", "", 44)
-
-        const expectedStations = ["KINGSTON", "OSHAWA"]
-        const expectedOrigins = ["TORONTO UNION STATION"]
-        const expectedTrainNumbers = [44, 52, 645]
 
         expect(actual.availableStations).toEqual(expectedStations)
         expect(actual.availableOrigins).toEqual(expectedOrigins)
